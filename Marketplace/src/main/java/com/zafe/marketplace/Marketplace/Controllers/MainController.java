584c625578c4ee6f40bf95ee4cf5392047ce84d8
PKKostov18 <PKKostov18@codingburgas.bg>
2023-03-06 14:37:12 +0200

    Create index file and connect it with the Java code via Thymeleaf Close #3

--- Marketplace/src/main/java/com/zafe/marketplace/Marketplace/Controllers/MainController.java
+++ Marketplace/src/main/java/com/zafe/marketplace/Marketplace/Controllers/MainController.java
@@ -1,4 +1,13 @@
 package com.zafe.marketplace.Marketplace.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping
+    public String indexPage() {
+        return "index";
+    }
 }
