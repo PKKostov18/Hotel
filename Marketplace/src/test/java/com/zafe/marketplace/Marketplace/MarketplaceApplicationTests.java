1879993da0dd9167d57febf80da8f8040ef25a2a
PKKostov18 <PKKostov18@codingburgas.bg>
2023-03-06 11:53:01 +0200

    Upload main spring boot project Close #1

---
+++ Marketplace/src/test/java/com/zafe/marketplace/Marketplace/MarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zafe.marketplace.Marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
